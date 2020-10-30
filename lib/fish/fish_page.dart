a8a28bd4887a838b7ab72261ab06704d9052ba15
Kananat Suwanviwatana <kananat.s@gmail.com>
2020-10-31 08:58:26 +0900

    wip

--- lib/fish/fish_page.dart
+++ lib/fish/fish_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:acnh/common/my_drawer.dart';
 import 'package:acnh/fish/fish.dart';
 import 'package:acnh/fish/fish_filter.dart';
@@ -14,7 +16,6 @@ class FishPage extends StatefulWidget {
 
 class _FishPageState extends State<FishPage> {
   Future<List<Fish>> _getFishes;
-
   FishFilterCondition _condition = FishFilterCondition();
 
   @override
@@ -22,6 +23,8 @@ class _FishPageState extends State<FishPage> {
     super.initState();
 
     _getFishes = modules.fishRepository.getFishes();
+    //_getFishes.then((value) => _fishes = value);
+    //_getFishes.then((value) => _isSelected = value.map((e) => false));
   }
 
   @override
@@ -56,10 +59,12 @@ class _FishPageState extends State<FishPage> {
                   ? Expanded(
                       child: ListView.separated(
                         shrinkWrap: true,
+                        padding: EdgeInsets.all(12),
                         itemCount: snapshot.data.length,
                         itemBuilder: (context, index) =>
                             _listViewItem(snapshot.data[index]),
-                        separatorBuilder: (context, index) => Container(),
+                        separatorBuilder: (context, index) =>
+                            SizedBox(height: 12),
                       ),
                     )
                   : Center(child: CircularProgressIndicator()),
@@ -68,16 +73,80 @@ class _FishPageState extends State<FishPage> {
         ),
       );
 
-  Widget _listViewItem(Fish fish) => Container(
-        child: ListTile(
-          leading: SizedBox(
-            height: 50,
-            width: 50,
-            child: Image.network(fish.iconUri),
+  Widget _listViewItem(Fish fish) => Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.lightBlue[300]),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: ListTile(
+              leading: SizedBox(
+                height: 50,
+                width: 50,
+                child: Image.file(File(fish.iconUri)),
+              ),
+              title: Text(fish.name.capitalized()),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.calendar_today,
+                        size: 16,
+                        color: Colors.grey[700],
+                      ),
+                      SizedBox(width: 4),
+                      Text("Mar ~ Sep"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.timelapse,
+                        size: 16,
+                        color: Colors.grey[700],
+                      ),
+                      SizedBox(width: 4),
+                      Text("9AM ~ 12AM"),
+                    ],
+                  ),
+                ],
+              ),
+              isThreeLine: true,
+              trailing: Column(
+                children: [
+                  RaisedButton(
+                    child: Text("Caught"),
+                    onPressed: () => _setCaught(fish),
+                  ),
+                ],
+              ),
+              //subtitle: Text(fis),
+            ),
           ),
-          title: Text(fish.name.capitalized()),
-          //subtitle: Text(fis),
-        ),
+          Positioned(
+            left: -10,
+            top: -10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(24),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
+              ),
+              child: Icon(
+                Icons.check,
+                color: Colors.white,
+                size: 24,
+              ),
+            ),
+          )
+        ],
       );
 
   Future<void> _showFishFilter(BuildContext context) async {
@@ -86,6 +155,15 @@ class _FishPageState extends State<FishPage> {
       pageBuilder: (context, animation, secondaryAnimation) => FishFilter(),
     );
 
-    if (condition != null) setState(() => _condition = condition);
+    if (condition != null)
+      setState(
+        () => _condition = condition,
+      );
+  }
+
+  Future<void> _setCaught(Fish fish) async {
+    fish.isCaught = true;
+    await modules.fishRepository.updateFish(fish);
+    setState(() {});
   }
 }
