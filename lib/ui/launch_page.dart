df3815d1c751a0f73cdc25df8a66e9f28a7d4be1
Kananat Suwanviwatana <kananat.s@gmail.com>
2020-11-07 00:48:42 +0900

    download on start

--- lib/ui/launch_page.dart
+++ lib/ui/launch_page.dart
@@ -1,4 +1,5 @@
 import 'package:acnh/bloc/bloc.dart';
+import 'package:acnh/bloc/bug/bug_bloc.dart';
 import 'package:acnh/bloc/fish/fish_bloc.dart';
 import 'package:acnh/ui/fish/fish_page.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,47 @@ class LaunchPage extends StatefulWidget {
 }
 
 class _LaunchPageState extends State<LaunchPage> with BlocProviderMixin {
+  bool _isFishReady = false;
+  bool _isBugReady = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: _fishListener(
+          child: _bugListener(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ),
+      );
+
+  Widget _fishListener({Widget child}) => BlocListener<FishBloc, FishState>(
+        listener: (context, state) {
+          if (state is ReadyFishState) _isFishReady = true;
+          _navigateIfReady(context);
+        },
+        child: child,
+      );
+
+  Widget _bugListener({Widget child}) => BlocListener<BugBloc, BugState>(
+        listener: (context, state) {
+          if (state is ReadyBugState) _isBugReady = true;
+          _navigateIfReady(context);
+        },
+        child: child,
+      );
+
+  void _navigateIfReady(BuildContext context) {
+    if (_isFishReady && _isBugReady) {
+      _isFishReady = false;
+      _isBugReady = false;
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => FishPage()),
+      );
+    }
+  }
+
+  /*
   @override
   Widget build(BuildContext context) => BlocConsumer<FishBloc, FishState>(
         listener: (context, state) {
@@ -25,4 +67,5 @@ class _LaunchPageState extends State<LaunchPage> with BlocProviderMixin {
           ),
         ),
       );
+      */
 }
